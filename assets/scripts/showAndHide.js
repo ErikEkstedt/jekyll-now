e8000ec9ded540c8152d52b5eca6d72661517705
Erik <eeckee@gmail.com>
2019-12-21 00:50:21 +0100

    update. really ugly js script for fast show/hide. super ugly. updates on cnn

---
+++ assets/scripts/showAndHide.js
@@ -0,0 +1,38 @@
+
+function cnnModule() {
+  var x = document.getElementById("cnnModule");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function cnnStack() {
+  var x = document.getElementById("cnnStack");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function nonSense() {
+  var x = document.getElementById("nonSense");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+// function showAndHide() {
+//   var x = document.getElementByClass("showAndHide");
+//   for (var i = 0, len = Things.length; i < len; i++) {
+  // }
+  // var x = document.getElementById("cnnStack");
+  // if (x.style.display === "none") {
+  //   x.style.display = "block";
+  // } else {
+  //   x.style.display = "none";
+  // }
+// }
